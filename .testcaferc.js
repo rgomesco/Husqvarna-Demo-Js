2b65692e68a7a12e0f8f67612db1e4784079b63b
rgomesco <rudolfcoutinho@gmail.com>
2025-07-30 01:12:26 +0530

    updated screenshot pathPattern in config file

--- .testcaferc.js
+++ .testcaferc.js
@@ -16,7 +16,7 @@ module.exports = {
     screenshots: {
         takeOnFails: true,
         path: "./reports/screenshots",
-        pathPattern: "${DATE}_${TIME}/${USERAGENT}/${TEST}.png"
+        pathPattern: "${DATE}_${TIME}/${USERAGENT}/${TEST}/${FILE_INDEX}.png"
     },
     videoPath: "./reports/videos",
     videoOptions: {
